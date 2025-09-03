7154edbd9836a251e1f2f7d5118a633bc8471ad4
PX-Desktop-5090-1 <developer@proximityrobotics.com>
2025-09-03 10:07:55 +0200

    for local test

--- algorithms/offline/cql.py
+++ algorithms/offline/cql.py
@@ -23,7 +23,7 @@ TensorBatch = List[torch.Tensor]
 
 @dataclass
 class TrainConfig:
-    device: str = "cuda:1"
+    device: str = "cuda"
     env: str = "halfcheetah-medium-expert-v2"  # OpenAI gym environment name
     seed: int = 0  # Sets Gym, PyTorch and Numpy seeds
     eval_freq: int = int(5e2)  # How often (time steps) we evaluate
